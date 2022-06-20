eb07c39dcfe67a9218a36e1f6ae555db0a848ba2
__zender__ <21009200891@stu.xidian.edu.cn>
2022-06-20 21:55:42 +0800

    fix

--- UniversalPowerupSystem/UniversalSys.cs
+++ UniversalPowerupSystem/UniversalSys.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using BepInEx;
 using BepInEx.Logging;
 using flanne;
 using flanne.Core;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
 namespace UniversalPowerupSystem
 {
+    public static class CustomPowerupPoolHelper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<Powerup> ToPowerupEnum(this IEnumerable<CustomPowerup> list) =>
+            list.Select(powerup => powerup.Powerup);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool ContainsPowerup(this IEnumerable<CustomPowerup> list, Powerup p) =>
+            list.Select(powerup => powerup.Powerup).Contains(p);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsRepeatable(this CustomPowerup customPowerup) => customPowerup.Powerup.isRepeatable;
+    }
+
+    [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
     public class CustomPowerup
     {
         public delegate void PreActionDelegate(CustomPowerup powerup);
@@ -25,7 +43,7 @@ namespace UniversalPowerupSystem
 
         public Powerup Powerup;
         public float Weight;
-        public float RawWeight;
+        public readonly float RawWeight;
 
         public Cond Condition;
         public ReLoad Reload;
@@ -44,6 +62,7 @@ namespace UniversalPowerupSystem
             BackupPreAction = backupPreAction;
         }
 
+
         #region Equal
 
         private sealed class PowerupEqualityComparer : IEqualityComparer<CustomPowerup>
@@ -59,7 +78,7 @@ namespace UniversalPowerupSystem
 
             public int GetHashCode(CustomPowerup obj)
             {
-                return (obj.Powerup != null ? obj.Powerup.GetHashCode() : 0);
+                return obj.Powerup != null ? obj.Powerup.GetHashCode() : 0;
             }
         }
 
@@ -74,13 +93,15 @@ namespace UniversalPowerupSystem
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((CustomPowerup) obj);
+            return obj.GetType() == GetType() && Equals((CustomPowerup) obj);
         }
 
         public override int GetHashCode()
         {
-            return (Powerup != null ? Powerup.GetHashCode() : 0);
+            unchecked
+            {
+                return ((Powerup != null ? Powerup.GetHashCode() : 0) * 397) ^ RawWeight.GetHashCode();
+            }
         }
 
         #endregion
@@ -89,65 +110,109 @@ namespace UniversalPowerupSystem
     [BepInPlugin("UniversalPowerupSystem.UniversalSys", "通用升级系统", "1.0.0")]
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     [SuppressMessage("ReSharper", "RedundantAssignment")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     public class UniversalSys : BaseUnityPlugin
     {
         private static readonly ManualLogSource UniversalSysModLogger =
             BepInEx.Logging.Logger.CreateLogSource("UniversalSys");
 
-        public static bool RegCustomPowerup(CustomPowerup customPowerup)
-        {
-            if (RawCustomPowerupPool.Contains(customPowerup)) return false;
-            RawCustomPowerupPool.Add(customPowerup);
-            return true;
-        }
-        
         public static bool RegCustomPowerup(Powerup powerup)
         {
-            if (RawCustomPowerupPool.Select(customPowerup => customPowerup.Powerup).Contains(powerup)) return false;
+            if (powerup.isRepeatable || RawCustomPowerupPool.ContainsPowerup(powerup)) return false;
             RawCustomPowerupPool.Add(new CustomPowerup(powerup));
             return true;
         }
 
+        public static bool RegCustomPowerup(CustomPowerup customPowerup)
+        {
+            if (customPowerup.IsRepeatable() || RawCustomPowerupPool.Contains(customPowerup)) return false;
+            RawCustomPowerupPool.Add(customPowerup);
+            return true;
+        }
+
+        public static bool RegRepeatableCustomPowerup(Powerup powerup)
+        {
+            if (RawRepeatableCustomPowerupPool.ContainsPowerup(powerup)) return false;
+            RawRepeatableCustomPowerupPool.Add(new CustomPowerup(powerup));
+            return true;
+        }
+
+        public static bool RegRepeatableCustomPowerup(CustomPowerup powerup)
+        {
+            if (RawRepeatableCustomPowerupPool.Contains(powerup)) return false;
+            RawRepeatableCustomPowerupPool.Add(powerup);
+            return true;
+        }
+
         public static readonly List<CustomPowerup> RawCustomPowerupPool = new List<CustomPowerup>();
-        public static int OncePowerupCount;
+        public static readonly List<CustomPowerup> RawRepeatableCustomPowerupPool = new List<CustomPowerup>();
         public static readonly List<CustomPowerup> RunnerPowerupPool = new List<CustomPowerup>();
         public static readonly List<CustomPowerup> TakenPool = new List<CustomPowerup>();
-        public static int OnceCount;
         public static GameController ThisGame;
         public const int StdWeight = 10;
 
         private static void VerifyPowerups()
         {
-            RawCustomPowerupPool.AddRange(from p in PowerupGenerator.Instance.powerupPool
-                where !RawCustomPowerupPool.Select(customPowerup => customPowerup.Powerup).Contains(p)
+            RawCustomPowerupPool.AddRange(
+                from p in PowerupGenerator.Instance.powerupPool ?? PowerupGenerator.Instance.profile.powerupPool
+                where !RawCustomPowerupPool.ContainsPowerup(p) &&
+                      !p.isRepeatable
                 select new CustomPowerup(p));
-            foreach (var powerup in RawCustomPowerupPool.Select(powerup => powerup.Powerup)
-                         .Where(powerup => !PowerupGenerator.Instance.powerupPool.Contains(powerup)))
-            {
-                RawCustomPowerupPool.RemoveAll(customPowerup => customPowerup.Powerup == powerup);
-            }
+            RawRepeatableCustomPowerupPool.AddRange(
+                from p in PowerupGenerator.Instance.powerupPool ?? PowerupGenerator.Instance.profile.powerupPool
+                where !RawRepeatableCustomPowerupPool.ContainsPowerup(p) &&
+                      p.isRepeatable
+                select new CustomPowerup(p, condition: StdRepeatableCondition));
         }
 
-        public static readonly CustomPowerup.Cond StdCondition = powerup =>
-            powerup.Powerup.prereqs.Count == 0 && !powerup.Powerup.isRepeatable
-            ||
-            powerup.Powerup.prereqs.Count != 0
-            &&
-            (!powerup.Powerup.anyPrereqFulfill && powerup.Powerup.prereqs.All(powerup1 =>
-                 TakenPool.Select(customPowerup => customPowerup.Powerup).Contains(powerup1)) ||
-             powerup.Powerup.anyPrereqFulfill && powerup.Powerup.prereqs.Any(powerup1 =>
-                 TakenPool.Select(customPowerup => customPowerup.Powerup).Contains(powerup1)))
-            &&
-            !TakenPool.Contains(powerup)
-            || (powerup.Powerup.isRepeatable && OncePowerupCount < OnceCount + GameState.numPowerupChoices &&
-                !RunnerPowerupPool.Contains(powerup));
+        private void Start()
+        {
+            BlockActions.Add(health => false);
+        }
+
+        public static readonly CustomPowerup.Cond StdCondition = NotRepeatableCond;
+        public static readonly CustomPowerup.Cond StdRepeatableCondition = RepeatableCond;
+
+        public static bool NotRepeatableCond(CustomPowerup powerup)
+        {
+            if (TakenPool.Contains(powerup) || PowerupGenerator.Instance.takenPowerups.Contains(powerup.Powerup) ||
+                powerup.IsRepeatable())
+            {
+                return false;
+            }
+
+            switch (powerup.Powerup.prereqs.Count == 0)
+            {
+                case true:
+                    return true;
+                case false:
+                    switch (powerup.Powerup.anyPrereqFulfill)
+                    {
+                        case true:
+                            return powerup.Powerup.prereqs.Any(powerup1 =>
+                                TakenPool.ContainsPowerup(powerup1));
+                        case false:
+                            return powerup.Powerup.prereqs.All(powerup1 =>
+                                TakenPool.ContainsPowerup(powerup1));
+                    }
+
+                    break;
+            }
+
+            return false;
+        }
+
+        public static bool RepeatableCond(CustomPowerup powerup)
+        {
+            return powerup.IsRepeatable();
+        }
 
         public static readonly CustomPowerup.ReLoad StdReload = powerup =>
         {
             TakenPool.Clear();
             RunnerPowerupPool.Clear();
             powerup.Weight = powerup.RawWeight;
-            OnceCount = 0;
         };
 
         public static readonly CustomPowerup.PreActionDelegate StdPreAction = null;
@@ -158,20 +223,24 @@ namespace UniversalPowerupSystem
         public static Sprite NullIcon;
         public static GameState GameState;
 
+        static UniversalSys()
+        {
+            if (!File.Exists($"{Paths.PluginPath}/Addition/powerupicon.icons")) return;
+            IconAsset = AssetBundle.LoadFromFile($"{Paths.PluginPath}/Addition/powerupicon.icons");
+            NullIcon = LoadAsset<Sprite>("NullIcon");
+        }
+        
+        public static T LoadAsset<T>(string name)
+            where T : Object
+        {
+            return IconAsset.LoadAsset<T>(name);
+        }
+        
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GameState), "Awake")]
         public static void GameStateAwake(GameState __instance)
         {
             GameState = __instance;
-        }
-
-        private void Awake()
-        {
-            if (File.Exists($"{Paths.PluginPath}/Addition/powerupicon.icons"))
-            {
-                IconAsset = AssetBundle.LoadFromFile($"{Paths.PluginPath}/Addition/powerupicon.icons");
-                NullIcon = IconAsset.LoadAsset<Sprite>("NullIcon");
-            }
         }
 
         private static bool RawPoolBuild;
@@ -180,35 +249,56 @@ namespace UniversalPowerupSystem
         [HarmonyPatch(typeof(GameController), "Start")]
         public static void InitOnGameStart(GameController __instance)
         {
-            VerifyPowerups();
             ThisGame = __instance;
-            foreach (var customPowerup in RawCustomPowerupPool)
-            {
-                customPowerup.Reload(customPowerup);
-            }
+
+            RawCustomPowerupPool.ForEach(powerup => powerup.Reload(powerup));
+            RawRepeatableCustomPowerupPool.ForEach(powerup => powerup.Reload(powerup));
 
             if (RawPoolBuild) return;
             RawCustomPowerupPool.AddRange(from p in PowerupGenerator.Instance.profile.powerupPool
+                where !p.isRepeatable
                 select new CustomPowerup(p));
-            foreach (var _ in PowerupGenerator.Instance.profile.powerupPool.Where(powerup => !powerup.isRepeatable))
-            {
-                OncePowerupCount += 1;
-            }
-
+            RawRepeatableCustomPowerupPool.AddRange(from powerup in PowerupGenerator.Instance.profile.powerupPool
+                where powerup.isRepeatable
+                select new CustomPowerup(powerup, condition: StdRepeatableCondition));
             RawPoolBuild = true;
+        }
+
+        public static void RollPowerupInPool(ref List<Powerup> __result, List<CustomPowerup> PowerupPool, int num,
+            bool isRepeatable = false)
+        {
+            for (var i = 0; i < num; i++)
+            {
+                var SumWeight = PowerupPool.Sum(powerup => powerup.Weight);
+                while (true)
+                {
+                    var pool = __result;
+                    if (PowerupPool.All(powerup => pool.Contains(powerup.Powerup)))
+                    {
+                        break;
+                    }
+
+                    var randomV = Random.Range(0, SumWeight);
+                    var randomPowerup = PowerupPool.FirstOrDefault(powerup =>
+                    {
+                        randomV -= powerup.Weight;
+                        return randomV <= 0;
+                    });
+
+                    if (randomPowerup == null || __result.Contains(randomPowerup.Powerup)) continue;
+                    if (isRepeatable && !randomPowerup.Condition(randomPowerup)) continue;
+                    __result.Add(randomPowerup.Powerup);
+                    break;
+                }
+            }
         }
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(PowerupGenerator), "GetRandom", typeof(int))]
         public static bool RollPowerup(PowerupGenerator __instance, ref List<Powerup> __result, int num)
         {
+            VerifyPowerups();
             __result = new List<Powerup>();
-            foreach (var customPowerup in RawCustomPowerupPool.Where(customPowerup =>
-                         customPowerup.Condition(customPowerup)))
-            {
-                RunnerPowerupPool.Add(customPowerup);
-                customPowerup.PreAction?.Invoke(customPowerup);
-            }
 
             var rmList = RunnerPowerupPool.Where(customPowerup => !customPowerup.Condition(customPowerup)).ToList();
 
@@ -218,23 +308,15 @@ namespace UniversalPowerupSystem
                 RunnerPowerupPool.Remove(customPowerup);
             }
 
-            for (var i = 0; i < Math.Min(num, RunnerPowerupPool.Count); i++)
+            foreach (var customPowerup in RawCustomPowerupPool.Where(customPowerup =>
+                         customPowerup.Condition(customPowerup)))
             {
-                var SumWeight = RunnerPowerupPool.Sum(powerup => powerup.Weight);
-                while (true)
-                {
-                    var randomV = Random.Range(0, SumWeight);
-                    var randomPowerup = RunnerPowerupPool.FirstOrDefault(powerup =>
-                    {
-                        randomV -= powerup.Weight;
-                        return randomV <= 0;
-                    });
-                    if (randomPowerup == null || __result.Contains(randomPowerup.Powerup)) continue;
-                    __result.Add(randomPowerup.Powerup);
-
-                    break;
-                }
+                RunnerPowerupPool.Add(customPowerup);
+                customPowerup.PreAction?.Invoke(customPowerup);
             }
+
+            RollPowerupInPool(ref __result, RunnerPowerupPool, Math.Min(num, RunnerPowerupPool.Count));
+            RollPowerupInPool(ref __result, RawRepeatableCustomPowerupPool, num - __result.Count, true);
 
             for (var i = __result.Count; i < num; i++)
             {
@@ -249,12 +331,68 @@ namespace UniversalPowerupSystem
         [HarmonyPatch(typeof(PowerupGenerator), "RemoveFromPool")]
         public static void MonitorRemove(Powerup powerup)
         {
-            var customPowerups = RawCustomPowerupPool.FindAll(customPowerup => customPowerup.Powerup == powerup);
-            TakenPool.AddRange(customPowerups.Where(customPowerup => !customPowerup.Powerup.isRepeatable));
-            if (!powerup.isRepeatable)
-            {
-                OnceCount += 1;
-            }
+            TakenPool.AddRange(RawCustomPowerupPool.FindAll(customPowerup => customPowerup.Powerup == powerup));
         }
+
+        #region Block
+
+        public static readonly List<Func<Health, bool>> BlockActions = new List<Func<Health, bool>>();
+        public static readonly List<Func<HPChangeData, bool>> EndPassActions = new List<Func<HPChangeData, bool>>();
+        public static readonly List<Func<Health, bool>> PlayerBlockActions = new List<Func<Health, bool>>();
+        public static readonly List<Func<HPChangeData, int>> ModifierActions = new List<Func<HPChangeData, int>>();
+        public static readonly List<Func<Health, float>> MulModifierActions = new List<Func<Health, float>>();
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Health), "HPChange")]
+        public static bool Health_HPChange(Health __instance, ref int change)
+        {
+            if (change < 0 && __instance.tag == "Player" &&
+                PlayerBlockActions.Any(blockAction => blockAction(__instance)))
+            {
+                change = 0;
+            }
+
+            if (change == 0) return true;
+
+            if (change < 0 && BlockActions.Any(blockAction => blockAction(__instance)))
+            {
+                change = 0;
+            }
+
+            if (change == 0) return true;
+
+            var hpChangeData = new HPChangeData
+            {
+                Change = change,
+                Health = __instance,
+                IsPlayer = __instance.tag == "Player"
+            };
+
+            foreach (var modifierAction in ModifierActions)
+            {
+                change += modifierAction(hpChangeData);
+                hpChangeData.Change = change;
+            }
+
+            change = (int) (change * MulModifierActions.Aggregate(1f,
+                (current, mulModifierAction) => current * mulModifierAction(__instance)));
+            if (__instance.tag != "Player" && change < 0)
+            {
+                change = Math.Min(change, -1);
+            }
+
+            if (!EndPassActions.Any(endPassAction => endPassAction(hpChangeData))) return true;
+            change = 0;
+            return true;
+        }
+
+        public class HPChangeData
+        {
+            public int Change;
+            public Health Health;
+            public bool IsPlayer;
+        }
+
+        #endregion
     }
 }
